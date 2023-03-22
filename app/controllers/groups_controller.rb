61eb981ef71872b9eb6a7f783dc7f10fae633961
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-03-21 20:15:17 -0400

    Create the page to add a category and display the categories

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,33 @@
+class GroupsController < ApplicationController
+  before_action :set_user
+
+  def index
+    @groups = @user.groups.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    puts group_params.inspect
+    @group = Group.new(group_params)
+    @group.user = @user
+    if @group.save
+      redirect_to groups_path, notice: 'Category added successfully'
+    else
+      flash.now[:alert] = @group.errors.full_messages.first if @group.errors.any?
+      render :new, status: 400
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_scope :user do
+    authenticated :user do
+      root 'groups#index', as: :authenticated_root
+    end
+    
+    unauthenticated do
+      root 'splash_screen#index', as: :unauthenticated_root
+    end
+  end  
+
+  resources :groups, only: [:index, :new, :create, :show, :destroy] do
+    resources :expenses, only: [:index, :new, :create, :show, :destroy]
+  end
 end
